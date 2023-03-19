6108ba1f474408bff9d4032ed687a2eb10e4f490
Denys Tantsiura <ghost0002501@gmail.com>
2023-03-19 23:35:35 +0200

    First example - Ok.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from authentication import get_password
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +80,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # https://www.alpharithms.com/django-postgresql-install-tutorial-123816/
+        # .postgresql_psycopg2 before v.1.9. The old name will continue to be available for backward compatibility.
+        'ENGINE': 'django.db.backends.postgresql',  # django.db.backends.postgresql_psycopg2 before v.1.9.
+        'NAME': 'scgkgtyo',  # DATABASE?
+        'USER': 'scgkgtyo',
+        'PASSWORD': get_password(),
+        'HOST': 'balarama.db.elephantsql.com',
+        # 'PORT': '5432',  # by default
+        'DATABASE': 'scgkgtyo',
     }
 }
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,8 +3,26 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'
+
 urlpatterns = [  # в якому будуть маршрути для нашого застосунку.
     # один маршрут під час звернення до застосунку polls і він оброблятиметься функцією index з файлу views.py. 
     # Цьому маршруту даємо ім'я index. Тепер кореневий каталог програми polls буде дозволений за допомогою views.index
+    # ex: /polls/
     path('', views.index, name='index'), 
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Додамо тепер обробник форм
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+'''
+Зверніть увагу на аргументи запиту, визначені у форматі <int:question_id>/. 
+Тепер шлях /polls/34/ буде оброблений представленням views.detail, куди 
+як аргумент question_id буде передано число 34. 
+Також ми додали ім'я застосунку app_name = 'polls'. Це необхідно, щоб 
+працювали конструкції виду <a href="{% url 'polls:detail' question.id %}">{{ question.question_text }}</a>
+'''
